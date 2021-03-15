76e7e294cae6888ec8dcd92711614008732ad33e
Chris <real.cr@gmx.net>
2021-03-15 22:13:48 +0100

    Ad button to refresh jlc status on smart filter page

--- LCSC_JLC_LIST.user.js
+++ LCSC_JLC_LIST.user.js
@@ -43,6 +43,11 @@ animation: spin 1s linear infinite;
         GetJLCStock();
     }
 
+    var myBTN = document.createElement ('div');
+    myBTN.innerHTML = '<button id="jlcbtn" style="padding: 10px 27px; color: white; margin: 30px; background-color: #16d; border: none; font-size: 15px;cursor: pointer;font-weight : bold ;border-radius: 4px;">Refresh JLC</button>';
+    let tmp=document.getElementsByClassName("category-result-area")[1];
+    tmp.parentNode.insertBefore(myBTN, tmp.nextSibling);
+    document.getElementById ("jlcbtn").addEventListener ("click", GetJLCStock, false);
 
     $(".pagination").bind("DOMSubtreeModified", function() {
         var span = document.getElementsByClassName("layui-laypage-curr")[0].getElementsByTagName("em")[1];
@@ -76,46 +81,52 @@ function GetJLCStock() {
         var stock = myrows[i].getElementsByClassName("avali-stock-tip")[0];
         var partnum = myrows[i].getElementsByClassName("part-num-title")[0].innerText;
         //console.log(i + ' - ' + partnum);
-        var clone = wait_element.cloneNode(true);
-        stock.appendChild(clone);
-        stock.id = 'jlc-stock#' + i
+        let tmp = myrows[i].getElementsByClassName("jlc-stock");
+        if (tmp.length > 0) {
+            //
+        } else {
 
-        GM.xmlHttpRequest({
-            method: 'GET',
-            url: url + partnum,
-            headers: {
-                'Content-type': 'application/json'
-            },
-            onload: function(xhr) {
-                var data = JSON.parse(xhr.responseText);
-                //console.log(data);
-                let res = data.result;
-                //console.log(res.component_code);
-                //console.log(res.stock_num);
-                var element = document.createElement("div");
-                element.classList.add("jlc-stock");
-                if (res.stock_num > 0) {
-                    element.classList.add('jlc-stock-good');
-                } else {
-                    element.classList.add('jlc-stock-bad');
-                }
+            var clone = wait_element.cloneNode(true);
+            stock.appendChild(clone);
+            stock.id = 'jlc-stock#' + i
 
-                element.innerHTML = 'JLC (<a href="' + jlc_ul + res.component_code + '" target="popup">' + res.component_code + '</a>) : <span id="jlc-stock">' + res.stock_num + '</span>';
-                for (var i = 0; i < myrows.length; i++) {
-                    var tmp_stock = myrows[i].getElementsByClassName("avali-stock-tip")[0];
-                    var tmp_partnum = myrows[i].getElementsByClassName("part-num-title")[0].innerText;
-                    if (tmp_partnum == res.component_code) {
-                        //tmp_stock.appendChild(element);
-                        tmp_stock.parentNode.insertBefore(element, tmp_stock.nextSibling);
-                        tmp_stock.getElementsByClassName("loader")[0].remove();
+            GM.xmlHttpRequest({
+                method: 'GET',
+                url: url + partnum,
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                onload: function(xhr) {
+                    var data = JSON.parse(xhr.responseText);
+                    //console.log(data);
+                    let res = data.result;
+                    //console.log(res.component_code);
+                    //console.log(res.stock_num);
+                    var element = document.createElement("div");
+                    element.classList.add("jlc-stock");
+                    if (res.stock_num > 0) {
+                        element.classList.add('jlc-stock-good');
+                    } else {
+                        element.classList.add('jlc-stock-bad');
                     }
-                }
-            },
-            onerror: function(xhr) {
-                //console.log("Error fetching JSON from xhr.");
-                return false;
-            }
-        });
 
+                    element.innerHTML = 'JLC (<a href="' + jlc_ul + res.component_code + '" target="popup">' + res.component_code + '</a>) : <span id="jlc-stock">' + res.stock_num + '</span>';
+                    for (var i = 0; i < myrows.length; i++) {
+                        var tmp_stock = myrows[i].getElementsByClassName("avali-stock-tip")[0];
+                        var tmp_partnum = myrows[i].getElementsByClassName("part-num-title")[0].innerText;
+                        if (tmp_partnum == res.component_code) {
+                            //tmp_stock.appendChild(element);
+                            tmp_stock.parentNode.insertBefore(element, tmp_stock.nextSibling);
+                            tmp_stock.getElementsByClassName("loader")[0].remove();
+                        }
+                    }
+                },
+                onerror: function(xhr) {
+                    //console.log("Error fetching JSON from xhr.");
+                    return false;
+                }
+            });
+
+        }
     }
 };
